efde925546e84599675e5607e84cdada6cac3e0b
Mukhtar <mukh.musaev@gmail.com>
2024-03-07 03:42:15 +0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-    domains: ['storage.googleapis.com'],
+  images: {
+    unoptimized: true,
   },
+  output: "export",
 };
 
 export default nextConfig;
